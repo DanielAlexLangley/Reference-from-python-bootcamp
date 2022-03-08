fde2409d5067f90837abf0883134f68e20b56823
Daniel <danielalexlangley@gmail.com>
2022-03-08 09:20:51 -0600

    final

--- apps_intermediate_plus/birthday_wisher/birthday_wisher_main.py
+++ apps_intermediate_plus/birthday_wisher/birthday_wisher_main.py
@@ -1,4 +1,26 @@
 
+import datetime
+import smtplib
+import random
 
-# Check if it's today
-# If it matches today's weekday, send email with random quote from quote.txt
+today = datetime.datetime.now()
+today_weekday = today.weekday()
+
+with open("birthday_wisher_quotes.txt") as data_file:
+    list_quotes = data_file.readlines()
+    quote_to_send = random.choice(list_quotes)
+
+sender = "Private Person <from@example.com>"
+receiver = "A Test User <to@example.com>"
+
+message = f"""\
+Subject: Inspiration quote 13
+To: {receiver}
+From: {sender}
+
+{quote_to_send}"""
+
+if today_weekday == 1:
+    with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
+        server.login("x", "x")
+        server.sendmail(sender, receiver, message)
