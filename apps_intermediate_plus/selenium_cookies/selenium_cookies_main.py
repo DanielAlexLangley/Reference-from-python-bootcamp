63dc79484afef8fc6cfb2548231c62600a5d5ae9
Daniel <danielalexlangley@gmail.com>
2022-03-17 21:28:28 -0500

    final

---
+++ apps_intermediate_plus/selenium_cookies/selenium_cookies_main.py
@@ -0,0 +1,32 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+import datetime
+
+endTime = datetime.datetime.now() + datetime.timedelta(minutes=5)
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+driver.implicitly_wait(10)
+driver.maximize_window()
+while True:
+    if datetime.datetime.now() >= endTime:
+        break
+
+    # CLick the cookie:
+    big_cookie = driver.find_element(by=By.ID, value="bigCookie")
+    for _ in range(0, 5):
+        big_cookie.click()
+
+    # Variable of how many cookies I have:
+    cookies = driver.find_element(by=By.ID, value="cookies").text
+
+    # Buy something:
+    for _ in range(0, 18):
+        item_store = driver.find_element(by=By.ID, value=f"product{0}")
+        if item_store.get_attribute("class") == "product unlocked enabled":
+            item_store.click()
+
+print(cookies)
+driver.quit()
