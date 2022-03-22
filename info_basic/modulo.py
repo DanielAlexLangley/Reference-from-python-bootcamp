ed27ea88a0910bd0cf984ba473accd5f933fb2be
Daniel <danielalexlangley@gmail.com>
2022-03-21 19:56:23 -0500

    final

--- info_basic/modulo.py
+++ info_basic/modulo.py
@@ -7,3 +7,7 @@ number2 = int(input("Second number: "))
 result1 = number1 / number2
 modulo1 = number1 % number2
 print(f"{number1} divided by {number2} equals {result1} with modulus {modulo1}")
+
+# It returns a whole number integer of the remainder.
+# If the number on the left is equal to the number on the right, it returns 0.
+# If the number on the left is less than the number on the right, it returns the number on the left.
